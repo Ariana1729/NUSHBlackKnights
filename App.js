9e3d1a5145a232372362669abb763c215330dd0f
Oolimry <limruiy@gmail.com>
2018-05-08 11:54:13 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,67 @@
 
 import React from 'react';
-import { Alert, AppRegistry, StyleSheet, Text, View, Button } from 'react-native';
+import { Alert, AppRegistry, StyleSheet, Text, View, Button, Animated, Dimensions } from 'react-native';
+
+var h = Dimensions.get('window').height;
+var w = Dimensions.get('window').width;
+
 
 export default class App extends React.Component {
+  constructor(){
+       super();
 
+       this.state = {
+            NavigationWidth: 0
+       }
+  }
+
+  chnageWidth = () => {
+       //Alert.alert("Hello World!");
+       this.setState({
+            NavigationWidth: w * 2 / 3
+       })
+ }
   render() {
+
+    var h = Dimensions.get('window').height;
+    var w = Dimensions.get('window').width;
+
+
     return (
-
       <View style={{
-          flexDirection: 'column',
-          flex: 1,
-          padding: 0
-        }}>
-        <View style={styles.container1}></View>
-        <View style={styles.container2}>
-          <Text style={styles.hamburgerButton}
-          onPress={() => {
-               Alert.alert('You tapped the button!');
-          }}> ≡</Text>
-          <Text style={styles.white}>    NUS High Black Knights</Text>
-        </View>
-        <View style={styles.container3}>
-          <Text style={styles.white}>Home page</Text>
-          <Text style={styles.white}>Announcements</Text>
-          <Text style={styles.white}>Schedule</Text>
-          <Text style={styles.white}>Menu</Text>
-        </View>
-      </View>
+         flexDirection: 'row',
+         flex: 1,
+         padding: 0
+       }}>
 
+
+           <View style={{
+               flexDirection: 'column',
+               flex: 1,
+               padding: 0
+             }}>
+             <View style={styles.container1}></View>
+             <View style={styles.container2}>
+               <Text style={styles.hamburgerButton}
+               onPress={this.chnageWidth}> ≡</Text>
+               <Text style={styles.white}>    NUS High Black Knights</Text>
+             </View>
+             <View style={styles.container3}>
+               <Text style={styles.white}>Home page</Text>
+               <Text style={styles.white}>Announcements</Text>
+               <Text style={styles.white}>Schedule</Text>
+               <Text style={styles.white}>Menu</Text>
+             </View>
+             </View>
+
+             <View style={{justifyContent:'center',alignItems:'center',alignSelf:'center', position:'absolute'}}>
+               <View style={{
+                  backgroundColor:'#1E1E3C',
+                  height: h,
+                  width: this.state.NavigationWidth,
+               }}></View>
+             </View>
+      </View>
     );
   }
 }
@@ -59,9 +93,12 @@ const styles = StyleSheet.create({
   hamburgerButton: {
       color: 'white',
       fontSize: 40,
- }
+  },
+
+  navigation: {
+
+  }
 
 });
 
 AppRegistry.registerComponent('TestApp2', () => App);
-
