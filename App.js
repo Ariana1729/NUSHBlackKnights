c79d9ffe8e37f5e12b85350c8935449b5908df7f
Oolimry <limruiy@gmail.com>
2018-05-27 20:05:50 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-//Dated 21 May 2018
+
 import React from 'react';
-import { Alert, AppRegistry, StyleSheet, Text, View, Button,
+import { Alert, AppRegistry, StyleSheet, Text, View, Button, Easing,
 Animated, Dimensions, TouchableOpacity, TouchableWithoutFeedback, FlatList } from 'react-native';
 import {StackNavigator, createStackNavigator} from 'react-navigation';
-import HomeScreen from './Screens/Home';
+
+import HomeScreen from './Screens/HomeScreen/Home';
+import AttendenceScreen from './Screens/AttendenceScreen/Attendence';
+import AnnouncementsScreen from './Screens/AnnouncementsScreen/Announcements';
+import GamesScreen from './Screens/GamesScreen/Games';
+import ProfileScreen from './Screens/ProfileScreen/Profile';
+import RecordScreen from './Screens/RecordScreen/Record';
+import ScheldueScreen from './Screens/ScheldueScreen/Scheldue';
+import TournamentsScreen from './Screens/TournamentsScreen/Tournaments';
+
 var h = Dimensions.get('window').height;
 var w = Dimensions.get('window').width;
 
+const transitionConfig = () => {
+  return {
+    transitionSpec: {
+      duration: 500,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: sceneProps => {
+      const { layout, position, scene } = sceneProps
+      const thisSceneIndex = scene.index
+      const width = layout.initWidth
+      const translateX = position.interpolate({
+        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        outputRange: [width, 0],
+      })
+      return { transform: [ { translateX } ] }
+    },
+  }
+}
+
 const RootStack = createStackNavigator(
      {
-     Home: {
-       screen: HomeScreen
-     },
+     Home: { screen: HomeScreen },
+     Attendence: { screen: AttendenceScreen },
+     Announcements: { screen: AnnouncementsScreen },
+     Games: { screen: GamesScreen },
+     Profile: { screen: ProfileScreen },
+     Record: { screen: RecordScreen },
+     Scheldue: { screen: ScheldueScreen },
+     Tournaments: { screen: TournamentsScreen },
 },
   {
     initialRouteName: 'Home',
-    /* The header config from HomeScreen is now here */
+    transitionConfig,
     headerMode: 'none',
-
   }
 );
 
@@ -28,4 +62,3 @@ export default class App extends React.Component {
 }
 
 AppRegistry.registerComponent('TestApp2', () => App);
-
